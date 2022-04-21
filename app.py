0466adc0b7acd0a5bd1e9e355b778f2eb3fa309f
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-04-21 12:09:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,11 @@ def nlp_unspv(df,column,cluster):
     return df
 
 ##TOP PAGE
-menu = ["TAHUB DATA","CLUSTERING","NEWS SCRAPER","FINANCIAL ANALYSIS", "PALM OIL MILLS MAPS"]
+st.title("Palm Oils Trafficking Risk Mapping Dashboard")
+st.markdown('<style>h1{color:dark-grey;font-size:62px}</style>',unsafe_allow_html=True)
+st.sidebar.image(Image.open('logo.png'))
+st.sidebar.image(Image.open('text.png'))
+menu = ["TAHUB DATA","CLUSTERING","NEWS SCRAPER","FINANCIAL ANALYSIS", "RISK MAPS"]
 choice = st.sidebar.selectbox("Select Menu", menu)
 if choice == "TAHUB DATA":
     components.html('''
@@ -135,20 +139,26 @@ elif choice =="FINANCIAL ANALYSIS":
     st.subheader('Worker Exploitation by Ratio Indicators')
     AgGrid(df)
 
-elif choice =="PALM OIL MILLS MAPS":
+elif choice =="RISK MAPS":
     components.html('''
-    <style>
-    #wrap { width: 1020px; height: 900px; padding: 0; border: 0px solid grey; overflow: hidden; }
-    #frame { width: 1680px; height: 900px;padding: 0; margin-top: -56px; border: 0px solid grey; overflow: hidden;}
-    </style>
-    <iframe id="frame" scrolling="no" class="wrapped-iframe" gesture="media"  allow="encrypted-media" allowfullscreen = "True"
-    name="Framename" sandbox="allow-same-origin allow-scripts allow-popups allow-forms" 
-    src="https://www.arcgis.com/home/webmap/viewer.html?useExisting=1&layers=3b28b8bcc5144cb685eb397979ea602f"
-    style="width: 100%;">
-    </iframe>
-    '''
-    ,height=1150,
-    width=1680)
+        <div class='tableauPlaceholder' id='viz1650513950841' style='position: relative'><noscript><a href='#'><img alt='Geographic-Specific Risk Profile ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;PalmOilIndustry-RiskAnalysis&#47;Geographic-SpecificRiskProfile&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='PalmOilIndustry-RiskAnalysis&#47;Geographic-SpecificRiskProfile' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;PalmOilIndustry-RiskAnalysis&#47;Geographic-SpecificRiskProfile&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650513950841');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1024px';vizElement.style.height='795px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1024px';vizElement.style.height='795px';} else { vizElement.style.width='100%';vizElement.style.height='1877px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+        ''',height=795,
+            width=1280)
+    maps_exp = st.expander('Palm oil mills maps')
+    with maps_exp:
+        components.html('''
+        <style>
+        #wrap { width: 1020px; height: 900px; padding: 0; border: 0px solid grey; overflow: hidden; }
+        #frame { width: 1680px; height: 900px;padding: 0; margin-top: -56px; border: 0px solid grey; overflow: hidden;}
+        </style>
+        <iframe id="frame" scrolling="no" class="wrapped-iframe" gesture="media"  allow="encrypted-media" allowfullscreen = "True"
+        name="Framename" sandbox="allow-same-origin allow-scripts allow-popups allow-forms" 
+        src="https://www.arcgis.com/home/webmap/viewer.html?useExisting=1&layers=3b28b8bcc5144cb685eb397979ea602f"
+        style="width: 100%;">
+        </iframe>
+        '''
+        ,height=795,
+        width=1150)
     
 
     # txt = st.text_input('Input Text Here')
